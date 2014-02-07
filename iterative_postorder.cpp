b33df110a84c0d91a92b1b36e1994a7c34f67451
pango89 <goyalpankaj10@live.com>
2014-02-07 22:55:29 +0530

    Update iterative_postorder.cpp

--- iterative_postorder.cpp
+++ iterative_postorder.cpp
@@ -2,19 +2,13 @@
 #include <stdio.h>
 #include <iostream>
 #include <stack>
-
 using namespace std;
-
-/* A binary tree node has data, left child and right child */
 struct node
 {
     int data;
     struct node* left;
     struct node* right;
 };
-
-/* Helper function that allocates a new node with the given data and
-   NULL left and right  pointers.*/
 struct node* newNode(int data)
 {
     struct node* node = new struct node;
@@ -23,54 +17,33 @@ struct node* newNode(int data)
     node->right = NULL;
     return(node);
 }
-
-// An iterative process to print preorder traversal of Binary tree
 void iterativePostorder(node *root)
 {
-    // Base Case
     if (root == NULL)
        return;
-
-    // Create an empty stack and push root to it
     stack<node *> nodeStack1;
 	stack<node *> nodeStack2;
     nodeStack1.push(root);
-	struct node *node=NULL;
-    /* Pop all items one by one. Do following for every popped item
-       a) print it
-       b) push its right child
-       c) push its left child
-    Note that right child is pushed first so that left is processed first */
+	struct node *node=NULL;    
     while (nodeStack1.empty() == false)
     {
-        // Pop the top item from stack and print it
         node = nodeStack1.top();
         nodeStack1.pop();
 		nodeStack2.push(node);
-        // Push right and left children of the popped node to stack
 		if (node->left)
             nodeStack1.push(node->left);
         if (node->right)
             nodeStack1.push(node->right);
     }
-	 while (nodeStack2.empty() == false)
+	while (nodeStack2.empty() == false)
     {
 		node = nodeStack2.top();
         nodeStack2.pop();
 		printf("%d ",node->data);
 	}
 }
-
-// Driver program to test above functions
 int main()
 {
-    /* Constructed binary tree is
-            10
-          /   \
-        8      2
-      /  \    /
-    3     5  2
-  */
   struct node *root = newNode(1);
   root->left        = newNode(2);
   root->right       = newNode(3);
