31ff294a7320443c8a4cf582d314dff87a70ce90
pango89 <goyalpankaj10@live.com>
2014-03-23 12:12:32 +0530

    Update inorder_successor_BST.c

--- inorder_successor_BST.c
+++ inorder_successor_BST.c
@@ -1,7 +1,7 @@
-1) If right subtree of node is not NULL, then succ lies in right subtree. Do following.
-Go to right subtree and return the node with minimum key value in right subtree.
-2) If right sbtree of node is NULL, then start from root and us search like technique. Do following.
-Travel down the tree, if a node’s data is greater than root’s data then go right side, otherwise go to left side.
+//1) If right subtree of node is not NULL, then succ lies in right subtree. Do following.
+//Go to right subtree and return the node with minimum key value in right subtree.
+//2) If right sbtree of node is NULL, then start from root and us search like technique. Do following.
+//Travel down the tree, if a node’s data is greater than root’s data then go right side, otherwise go to left side.
 struct node * inOrderSuccessor(struct node *root, struct node *n)
 {
     // step 1 of the above algorithm
