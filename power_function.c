012bca17e2f94353fcac3612cd037d6a57597529
pango89 <goyalpankaj10@live.com>
2014-03-26 17:04:41 +0530

    Update power_function.c

--- power_function.c
+++ power_function.c
@@ -11,6 +11,18 @@ long long int power(long long int base,int exp)
 	}
 	return result;
 }
+/* Function to calculate x raised to the power y in O(logn)*/
+int power1(int x, unsigned int y)
+{
+    int temp;
+    if( y == 0)
+        return 1;
+    temp = power(x, y/2);
+    if (y%2 == 0)
+        return temp*temp;
+    else
+        return x*temp*temp;
+}
 int main()
 {
 	long long int result=0;
